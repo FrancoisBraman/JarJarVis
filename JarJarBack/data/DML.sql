815bec9ea98889ab16885dbe2ba2d505c09e31bd
FrancoisBraman <francoisbraman@gmail.com>
2024-01-30 15:08:03 +0100

    db connectée

--- JarJarBack/data/DML.sql
+++ JarJarBack/data/DML.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 INSERT INTO "experience" 
-("starting_date", "ending_date", "job", "location", "description", "created_at", "updated_at") 
+("starting_date", "ending_date", "job", "location", "description") 
 VALUES 
 ('2008-06-30', '2012-10-15', 'multiples postes dans la restauration, de plongeur à chef de cuisine en passant par pizzaïolo et encore d''autres postes', 'dans différentes stations blanéaires des Landes et stations de ski des Pyrénées', 'Préparation des mets dans différents établissement, apprentissage des techniques culinaires, travail en équipe...'),
 ('2013-02-01', '2014-10-31', 'second de cuisine', 'restaurant "Le Bahia Lounge Café" à Saint-François en Guadeloupe(971)', 'mission de remise en état d''un restaurant en grande perte de vitesse. L''objectif était de rétablir un chiffre d''affaire suffisant pour pouvoir vendre l''affaire à un prix convenable. Création de carte, gestion des stocks, des équipes et des relations fournisseur, préparations culinaires etc...'),
@@ -11,14 +11,13 @@ VALUES
 ('2023-05-01', '2023-05-31', 'développeur full-stack', 'école O''clock à distance', 'Réalisation d''un remake de la version web du jeu de cartes "Arcomage" (saga Might&Magic) au sein d''une équipe de 5 personnes en méthodologie AGILE. Mise en place d''un serveur websocket et d''une API REST ainsi que d''une base de données PostgreSQL. UI réalisée avec REACT et SCSS. Utilisation de GitHub.');
 
 INSERT INTO "background" 
-("starting_date", "ending_date", "location", "description", "degree", "created_at", "updated_at")
+("starting_date", "ending_date", "location", "description", "degree")
 VALUES
 ('2005-09-01', '2008-05-30', 'ISEG de Toulouse(31)', 'Ecole de commerce, marketing et communication', 'Titre ISEFAC obtenu (bac+3)'),
 ('2011-04-01', '2011-09-30', 'INFREP de Mont-de-Marsan(40)', 'Centre de formation', 'CQP AGent de restauration obtenu'),
 ('2022-12-05', '2023-05-31', 'école O''clock à distance', 'Formation intensive de 5 mois au développement web full-stack', 'Titre RNCP de niveau 5 (bac+2) de développeur web et web mobile obtenu');
 
-INSERT INTO "stack" 
-("tech", "language_type", "created_at", "updated_at")
+INSERT INTO "stack"("tech", "language_type", "level")
 VALUES
 ('React', 'front-end','intermediate'),
 ('HTML', 'front-end','intermediate'),
@@ -29,8 +28,8 @@ VALUES
 ('NodeJS', 'back-end','intermediate'),
 ('Express', 'back-end','intermediate'),
 ('PostgreSQL', 'back-end','advanced'),
-('Sequelize', 'back-end',,'intermediate'),
-('GraphQL', 'back-end','beginner')
+('Sequelize', 'back-end','intermediate'),
+('GraphQL', 'back-end','beginner'),
 ('Socket.io', 'tool','beginner'),
 ('Sqitch', 'back-end','beginner'),
 ('EJS', 'back-end','intermediate'),
@@ -46,7 +45,7 @@ VALUES
 ('Spanish', 'language','intermediate');
 
 INSERT INTO "soft_skill" 
-("item", "created_at", "updated_at")
+("item")
 VALUES
 ('Travail en équipe'),
 ('Soif de connaître'),
@@ -55,12 +54,12 @@ VALUES
 ('Créativité');
 
 INSERT INTO "information" 
-("name", "birthdate", "mobile", "adress", "occupation", "link", "created_at", "updated_at")
+("name", "birthdate", "mobile", "adress", "occupation", "link")
 VALUES
-('François Braman', '1985-08-04', '06 82 28 94 75', '40 rue du général de Castelnau - 33700 Mérignac', 'Développeur web full-stack', 'LinkedIn : https://www.linkedin.com/in/françois-braman-a99766245/, GitHub : https://github.com/FrancoisBraman');
+('François Braman', '1985-08-04', '0682289475', '40 rue du général de Castelnau - 33700 Mérignac', 'Développeur web full-stack', 'LinkedIn : https://www.linkedin.com/in/françois-braman-a99766245/, GitHub : https://github.com/FrancoisBraman');
 
 INSERT INTO "hobby" 
-("item", "created_at", "updated_at")
+("item")
 VALUES 
 ('Technologie'),
 ('Astronomie'),
